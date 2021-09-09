a573c35f971ea70053542924988a15eb01f68148
paulo.oliveira <paulo.oliveira@sequor.com.br>
2021-09-09 13:43:06 -0300

    Adjustments for running with PM2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+const path = require('path'); 
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const tmi = require('tmi.js');
 
 const client = new tmi.Client({
